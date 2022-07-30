a2052c847b6205a19a7f38b7e84ef4251a16fe78
Jahan Zeb Ahmed <jjahanzebb@gmail.com>
2022-07-30 20:32:58 +0500

    Day 1 completed

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import {View, Text} from 'react-native';
+import {View, Text, SafeAreaView, Image} from 'react-native';
 import React, {useLayoutEffect} from 'react';
 import {useNavigation} from '@react-navigation/native';
 
@@ -12,9 +12,24 @@ const HomeScreen = () => {
   }, []);
 
   return (
-    <View>
-      <Text className="text-red-500 text-center">HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text className="text-red-500">
+        {/* Header */}
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image
+            source={{uri: 'https://links.papareact.com/wru'}}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+
+          <View>
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!
+            </Text>
+            <Text className="font-bold  text-xl">Current Location</Text>
+          </View>
+        </View>
+      </Text>
+    </SafeAreaView>
   );
 };
 
